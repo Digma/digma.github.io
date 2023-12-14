1d9f3f0a851d92c8428dc014fca8d9e0425ca123
Gael Grosch <gael.grosch@gmail.com>
2023-12-14 23:28:29 +0100

    feat: prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gael.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://gael.io",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Gael.io';
-export const SITE_DESCRIPTION = 'Welcome to Gael\'s Website!';
+export const SITE_TITLE = "Gael.io";
+export const SITE_DESCRIPTION = "Welcome to Gael's Website!";
