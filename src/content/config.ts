56aa08ce2b8729ed6337c81e7762a0f6212b977b
Gael Grosch <gael.grosch@gmail.com>
2024-01-12 16:26:37 +0100

    feat: push before weekend

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,16 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const products = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, products };
