2164b8bf3ba38ec4daa2ea8c70bdff15a2368491
Gael Grosch <gael.grosch@gmail.com>
2024-01-15 17:40:25 +0100

    feat: prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        'resume': ['Outfit', 'Open sans', 'sans-serif'],
-        'blog': ['Open sans', 'sans-serif'],
+        resume: ["Outfit", "Open sans", "sans-serif"],
+        blog: ["Open sans", "sans-serif"],
       },
       typography: {
         DEFAULT: {
           css: {
-            maxWidth: '860px',
-          }
-        }
-      }
+            maxWidth: "860px",
+          },
+        },
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
